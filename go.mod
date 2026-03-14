4e178143593f87a75b288adab5ccf9fbe682b6b2
DennisPrudlik <D.Prudlik97@gmail.com>
2026-03-14 16:29:01 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DennisPrudlik/chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
